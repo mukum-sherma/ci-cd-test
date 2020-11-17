ed2d24abc556efbba195042f31cc631d77136b9f
Sanjog_Limbu <laughingmukum@yahoo.com>
2020-11-17 11:14:30 +0800

    jenkins is awesome

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 console.log('Server starting...');
 http.createServer((req, res) => {
     console.log('Request received...');
-    res.end('Hey , Hello Sherma, How are you? Jenkins is setup now Great Good!!! ');
+    res.end('Jenkins is awesome');
 }).listen(80, () => {
     console.log('started.');
 });
