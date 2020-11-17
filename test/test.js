1d9e169941c70985acbb064a27d06a0c26f7837f
Sanjog_Limbu <laughingmukum@yahoo.com>
2020-11-17 15:22:38 +0800

    jenkins connects with node server

--- test/test.js
+++ test/test.js
@@ -2,19 +2,19 @@ var request = require("supertest");
 var app = require("../app.js");
 var assert = require('assert');
 
-// describe("GET /", function(){
-//     it('respond with hello world, good, great, yea 11', function(done){
+describe("GET /", function(){
+    it('respond with hello world, good, great, yea 11', function(done){
 
-//         //navigate to root and check the response is "hello world"
-//         request(app).get("/").expect("mukum",done);
-//     });
+        //navigate to root and check the response is "hello world"
+        request(app).get("/").expect("mukum",done);
+    });
 
-//     setTimeout(function () {
-//         console.log('timeout completed'); 
-//         process.exit();
-//     }, 3000);
+    setTimeout(function () {
+        console.log('timeout completed'); 
+        process.exit();
+    }, 3000);
     
-// });
+});
 
 describe('Array', function() {
     describe('#indexOf()', function() {
