58968cd4cd73391f9c4fca546eaa9fd182006b76
mukum-sherma <mukum.sherma@gmail.com>
2020-11-17 10:49:32 +0800

    jenkins setup 2

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const http = require('http');
 console.log('Server starting...');
 http.createServer((req, res) => {
     console.log('Request received...');
-    res.end('Hello Sherma, How are you? Jenkins is setup now ');
+    res.end('Hello Sherma, How are you? Jenkins is setup now Great!!! ');
 }).listen(80, () => {
     console.log('started.');
 });
