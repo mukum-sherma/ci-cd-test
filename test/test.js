d5620f30413c4d02d4be59b8678702d517630ae5
Sanjog_Limbu <laughingmukum@yahoo.com>
2020-11-17 14:28:34 +0800

    JENKINS, added --exit flag to mocha test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,25 @@
 var request = require("supertest");
 var app = require("../app.js");
+var assert = require('assert');
 
-describe("GET /", function(){
-    it('respond with hello world, good, great, yea 11', function(done){
+// describe("GET /", function(){
+//     it('respond with hello world, good, great, yea 11', function(done){
 
-        //navigate to root and check the response is "hello world"
-        request(app).get("/").expect("mukum",done);
-    });
+//         //navigate to root and check the response is "hello world"
+//         request(app).get("/").expect("mukum",done);
+//     });
 
-    setTimeout(function () {
-        console.log('timeout completed'); 
-        process.exit();
-    }, 3000);
+//     setTimeout(function () {
+//         console.log('timeout completed'); 
+//         process.exit();
+//     }, 3000);
     
-});
+// });
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+      it('should return -1 when the value is not present', function() {
+        assert.equal([1, 2, 3].indexOf(4), -1);
+      });
+    });
+  });
