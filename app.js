c79d8f41bdb7dcd9eb45bc39dd9ca2dcf02647a2
Sanjog_Limbu <laughingmukum@yahoo.com>
2020-11-17 12:34:37 +0800

    JENKINS, npm added in jenkins server

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get("/", function (req, res) {
  res.send("mukum");
 });
 //listen to port 3000 by default
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 3000);
  
 module.exports = app;
