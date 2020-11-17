95356ece8d3be4a3a93c229d7b1cfde3927e1b4a
Sanjog_Limbu <laughingmukum@yahoo.com>
2020-11-17 12:16:22 +0800

    JENKINS, npm added in jenkins server

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require("supertest");
 var app = require("../app.js");
 
 describe("GET /", function(){
-    it('respond with hello world, good', function(done){
+    it('respond with hello world, good, great', function(done){
 
         //navigate to root and check the response is "hello world"
         request(app).get("/").expect("mukum",done);
