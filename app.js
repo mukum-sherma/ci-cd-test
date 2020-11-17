1cc64e58f1c2e5a4eafef2e318dd367778d1e3e3
Sanjog_Limbu <laughingmukum@yahoo.com>
2020-11-17 11:16:12 +0800

    JENKINS IS GREAT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 console.log('Server starting...');
 http.createServer((req, res) => {
     console.log('Request received...');
-    res.end('Jenkins is awesome');
+    res.end('JENKINS IS GREAT');
 }).listen(80, () => {
     console.log('started.');
 });
