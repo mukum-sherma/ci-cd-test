bd55fe5449fe4ec0286e0fbba217504b31aadf3e
mukum-sherma <mukum.sherma@gmail.com>
2020-11-17 11:03:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// //importing node framework
-// var express = require("express");
- 
-// var app = express();
-// //Respond with "hello world" for requests that hit our root "/"
-// app.get("/", function (req, res) {
-//  res.send("CI CD Test - Mukum ");
-// });
-// //listen to port 3000 by default
-// app.listen(process.env.PORT || 3000, () =>{
-//     console.log("Listening on port : 3000")
-// });
- 
-// module.exports = app;
-
 
 const http = require('http');
 console.log('Server starting...');
